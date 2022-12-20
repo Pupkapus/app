8be1c66febe269afbb33f3726f91f01eccb48677
Pupkapus <hunter235@bk.ru>
2022-12-20 20:38:46 +1100

    new

---
+++ lab10/lb10.sql
@@ -0,0 +1,33 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema university
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema university
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `university` DEFAULT CHARACTER SET utf8 ;
+USE `university` ;
+
+-- -----------------------------------------------------
+-- Table `university`.`students`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `university`.`students` ;
+
+CREATE TABLE IF NOT EXISTS `university`.`students` (
+  `id` INT NULL,
+  `счетчик` INT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `d_id` INT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
